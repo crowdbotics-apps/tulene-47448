3cdf6625bac79c080dffa4044f5e082550fbdcdf
Crowdbotics <team@crowdbotics.com>
2024-04-10 18:10:32 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tulene-47448",
+  url: "https://tulene-47448.botics.co",
+  api: "https://tulene-47448.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
